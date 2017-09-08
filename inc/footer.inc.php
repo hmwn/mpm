facc834887b25b907ff3fe8094aeabc8497c9eaf
hmwn <31754387+hmwn@users.noreply.github.com>
2017-09-08 11:27:43 +0700

    Add files via upload

---
+++ inc/footer.inc.php
@@ -0,0 +1,23 @@
+
+
+<footer class="footer">
+	<div class="container-fluid">
+	</div>
+</footer>
+
+
+
+</body>
+
+<!--   Core JS Files   -->
+<script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!--  Notifications Plugin    -->
+<script src="assets/js/bootstrap-notify.js"></script>
+
+<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+<script src="assets/js/light-bootstrap-dashboard.js"></script>
+
+</script>
+</html>
---
+++ inc/header.inc.php
@@ -0,0 +1,35 @@
+<?php session_start();?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>AssesmentSample</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	<meta name="viewport" content="width=device-width" />
+
+	<!-- Bootstrap core CSS -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+	<!--  Light Bootstrap Table core CSS    -->
+<link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+
+	<!--     Fonts and icons     -->
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+	<link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+	<link href="inc/font.css" rel="stylesheet" />
+	<link href="assets/css/brand.css" rel="stylesheet" />
+</head>
+<body>
+
+		 <div id="navbar" class="navbar navbar-fixed-top" style="background-color:orange; padding-top:15px; width: 100%;">
+					<a href="#" class="simple-text" style="color:white; margin-top: 80px;">
+						<strong>Assesment</strong>Sample
+						<!--<img src="assets/img/MPM_Logo Group_White.png" class="img-responsive" alt="logo">-->
+						<div class="timer" style="float:right;  margin-right: 20px;">timer</div>
+					</a>
+				</div>
